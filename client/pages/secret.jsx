a3e3d5b334f833bc4397df7f3b9a9dc74d0f72f2
Jakob Booker <jakob.booker@colonyamerican.com>
2015-10-19 13:06:16 -0700

    hotfix to secrets page

--- client/pages/secret.jsx
+++ client/pages/secret.jsx
@@ -10,16 +10,27 @@ const SecretPage = React.createClass({
     let {userID} = this.context.router.getCurrentQuery();
     if (userID != 1) {
       this.replaceWith('/');
-    }
+    };
   },
 
   render() {
+    var color = {
+      color : 'green',
+    };
+
     return (
       <div className="container-fluid">
       	<Grid>
       		<Row>
               <Col md={12}>
-                <h5 className="simplepad"><a href='http://instantcena.com/'>Secret Link</a></h5>
+                <h4 className='simplepad'>Bank Account: </h4>
+                <div style={color}>$16,890,236,788.87</div>
+                <h4 className='simplepad'>Tools: </h4>
+                <ul>
+                  <li>Milk. Lots of milk.</li>
+                  <li>Danger Proof Milk Man Suit.</li>
+                  <li>1954 Danger Proof "Milk" Truck.</li>
+                </ul>
               </Col>
       		</Row>
         </Grid>
